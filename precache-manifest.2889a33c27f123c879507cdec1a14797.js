ee88dcd733a3c9b758d506ffd4b022e4377a2458
lauwinskyrichard <richard.lauwinsky@binus.ac.id>
2022-01-16 16:45:48 +0700

    Updates

---
+++ precache-manifest.2889a33c27f123c879507cdec1a14797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a4d65b8d802d105abb55e0ac34ad93",
+    "url": "/bmi-calculator-reactjs/index.html"
+  },
+  {
+    "revision": "91a743a3ad687a659892",
+    "url": "/bmi-calculator-reactjs/static/css/main.86306c03.chunk.css"
+  },
+  {
+    "revision": "d6f8a8a010f219426a5c",
+    "url": "/bmi-calculator-reactjs/static/js/2.a27aeb8f.chunk.js"
+  },
+  {
+    "revision": "2ed2a2b040e06c6731fd167cc037f177",
+    "url": "/bmi-calculator-reactjs/static/js/2.a27aeb8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "659f0961ab1110dbed9b",
+    "url": "/bmi-calculator-reactjs/static/js/3.119a374e.chunk.js"
+  },
+  {
+    "revision": "91a743a3ad687a659892",
+    "url": "/bmi-calculator-reactjs/static/js/main.a971fa33.chunk.js"
+  },
+  {
+    "revision": "6f55fce84c81f01a18a3",
+    "url": "/bmi-calculator-reactjs/static/js/runtime-main.62701ce8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bmi-calculator-reactjs/precache-manifest.2889a33c27f123c879507cdec1a14797.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bmi-calculator-reactjs/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
